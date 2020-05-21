2550341eecbaec9e3632b3ebd1240680a811df7f
djptek <dominic.page@elastic.co>
2020-05-21 16:33:19 +0200

    rename to match lab

--- scripts/parse_labs.rb
+++ scripts/parse_labs.rb
@@ -14,15 +14,18 @@ class Parsed_XML
           downcase
   end
 
-  # accessor for <code/> elements
-  def code_elements
-    #return @doc.xpath '//'
+  def h1_as_filename
+    return (@doc.xpath '//h1//text()').text.downcase.gsub(/\W/,'_')
+  end
+
+  # accessor for text elements
+  def text_nodes
     return @doc.xpath '//body//text()'
   end
     
   def traverse_elements
     solutions = ''
-    code_elements.each do |code|
+    text_nodes.each do |code|
       solutions << "######\n\n#{code.text}\n\n" if 
         code.path.end_with?( 'h1/text()', 'h2/text()') or
         code.text.start_with?( 'GET ', 'PUT ', 'POST ', 'DELETE ')
@@ -35,14 +38,15 @@ end
 ####
 # iterate over all XML files in local folder
 $source_dir = "./"
-$prefix     = "labs/*"
-$from_rhs   = ".html"
-$to_rhs     = "_solutions.txt"
+$prefix     = "labs/"
+$from_rhs   = "*.html"
+$to_rhs     = "_requests.txt"
 $target_dir = "./"
 Dir[$source_dir+$prefix+$from_rhs].each do |in_file| 
-  File.open(in_file.sub($from_rhs, $to_rhs), 'w') do |out_file| 
-    out_file.write(Parsed_XML.
-      new(source_filename: in_file).
+  parsed = Parsed_XML.
+      new(source_filename: in_file)
+  File.open($prefix+parsed.h1_as_filename+$to_rhs, 'w') do |out_file| 
+    out_file.write(parsed.
       traverse_elements)
   end
 end
