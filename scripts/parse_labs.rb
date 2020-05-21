f9633159ba82f0d8e23093f204a63aa9a00e5f4b
djptek <dominic.page@elastic.co>
2020-05-21 13:47:36 +0200

    tweaked for FFWD labs

--- scripts/parse_labs.rb
+++ scripts/parse_labs.rb
@@ -22,8 +22,8 @@ class Parsed_XML
   def traverse_elements
     solutions = ''
     code_elements.each do |code|
-      # ignore elements where no membercontribution present
-			solutions << "######\n\n#{code.text}\n\n"
+      solutions << "######\n\n#{code.text}\n\n" if 
+        code.text.start_with?( 'GET ', 'PUT ', 'POST ', 'DELETE ')
     end
     return solutions
   end
@@ -33,12 +33,14 @@ end
 ####
 # iterate over all XML files in local folder
 $source_dir = "./"
-$prefix     = "labs"
+$prefix     = "labs/*"
 $from_rhs   = ".html"
 $to_rhs     = "_solutions.txt"
 $target_dir = "./"
-Dir[$source_dir+$prefix+$from_rhs].each do |x| 
-	  #p "processing #{x}"
-		puts Parsed_XML.new(source_filename: x).
-			traverse_elements
+Dir[$source_dir+$prefix+$from_rhs].each do |in_file| 
+  File.open(in_file.sub($from_rhs, $to_rhs), 'w') do |out_file| 
+    out_file.write(Parsed_XML.
+      new(source_filename: in_file).
+      traverse_elements)
+  end
 end
